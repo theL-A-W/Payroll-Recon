6678de74db44b759e39f255113eb3f30d3ae03c4
Lindsay Walker <lawalker98@gmail.com>
2023-10-19 16:20:43 -0400

    end of workday update

--- client/history/history.js
+++ client/history/history.js
@@ -7,40 +7,37 @@ const numOfDates = 1
 function calculatePastDates(startDate, numberOfDates) {
 
       const pastDates = []
-     
       let currentDate = new Date(startDate);
     
       for (let i = 0; i < numberOfDates; i++) {
-        const formattedDate = `${currentDate.getMonth() + 1}/${currentDate.getDate()}/${currentDate.getFullYear()}`;
+        const formattedDate = `${currentDate.getMonth() + 1}/${currentDate.getDate()}/${currentDate.getFullYear()}`
         pastDates.push(formattedDate);
-    
-        // Add 7 days (one week) to the current date
-        currentDate.setDate(currentDate.getDate() - 7);
+        currentDate.setDate(currentDate.getDate() - 7)
       }
 
-  return pastDates;
+  return pastDates
 }
-const pastPayDays = calculatePastDates(startDate, numberOfDates);
-console.log('Past paydays:', pastPayDays);
+const pastPayDays = calculatePastDates(startDate, numberOfDates)
+console.log('Past paydays:', pastPayDays)
 
 //NEXT PAY DAYS
 function calculateNextDates(initialDate, numOfDates) {
       const nextDates = []
      
-      let currentDate = new Date(initialDate);
+      let currentDate = new Date(initialDate)
     
       for (let i = 0; i < numOfDates; i++) {
-        const formattedDate = `${currentDate.getMonth() + 1}/${currentDate.getDate()}/${currentDate.getFullYear()}`;
-        nextDates.push(formattedDate);
+        const formattedDate = `${currentDate.getMonth() + 1}/${currentDate.getDate()}/${currentDate.getFullYear()}`
+        nextDates.push(formattedDate)
     
         // Add 7 days (one week) to the current date
-        currentDate.setDate(currentDate.getDate() + 7);
+        currentDate.setDate(currentDate.getDate() + 7)
       }
 
   return nextDates;
 }
-const nextPayDays = calculateNextDates(initialDate, numOfDates);
-console.log('Next paydays:', nextPayDays);
+const nextPayDays = calculateNextDates(initialDate, numOfDates)
+console.log('Next paydays:', nextPayDays)
 
 
 
@@ -60,15 +57,24 @@ async function getPayments() {
   const response = await axios.get(`http://localhost:3001/payments`)
   return response.data
 }
-//RETURN ALL RUNS AND PAYMENTS
+//GET DRIVERS
+async function getDrivers() {
+  const response = await axios.get(`http://localhost:3001/drivers`)
+  return response.data
+}
+//RETURN ALL RUNS, PAYMENTS, DRIVERS
 async function yesRunsPayments() {
 
   const allRuns = await getRuns()
   console.log('All Runs', allRuns)
 
+  const allDrivers = await getDrivers()
+  console.log('All Drivers', allDrivers)
+
   const allPayments = await getPayments()
   console.log('All Payments', allPayments)
 
+  // enterBtn.addEventListener("click", () => {
   allPayments.forEach(payment => {
         let mainBox = document.getElementById('mainBox')
         let pastWeek = document.createElement('div')
@@ -85,60 +91,98 @@ async function yesRunsPayments() {
   
 
   })
+ 
 
-forEach(driver => {
-  //find all dates 7 days prior
-  //if date is 1 day prior to W/E, it is a friday, if it is 2 days prior on the numerical calendar, it is a thursday, etc.
-  //add all runTotals that are 7 days prior to the Week-Ending date
-//display total for each day
-//if day has no total, default pay to zero for that day
-  //post runTotal and driver name to "payments"
+//   //CREATE A PAY PERIOD AND ASSIGN EACH RUN TO A PAY PERIOD
+const payPeriods = []
+for (let i = 0; i < pastPayDays.length; i++) {
+  const payWeekEnd = new Date(pastPayDays[i])
+
+
+  const payWeekStart = new Date(payWeekEnd)
+  payWeekStart.setDate(payWeekStart.getDate() - 6)
+
+  payPeriods.push({
+    start: payWeekStart,
+    end: payWeekEnd,
+  })
+}
+
+const runsByPayPeriod = {}
+
+allRuns.forEach(run => {
+  const runDate = new Date(run.date)
+  let assignedToPayPeriod = false
+
+  for (let i = 0; i < payPeriods.length; i++) {
+    const payPeriod = payPeriods[i]
+
+    if (runDate >= payPeriod.start && runDate <= payPeriod.end) {
+      const payPeriodKey = `Week ${i + 1}`
+      
+      if (!runsByPayPeriod[payPeriodKey]) {
+        runsByPayPeriod[payPeriodKey] = []
+      }
+      console.log(payPeriodKey)
+      runsByPayPeriod[payPeriodKey].push(run)
+
+
+enterBtn.addEventListener("click", () => {
+        
+        const emp_name = run.emp_name
+        const baseFare = run.baseFare
+        const surcharge = run.surcharge
+        const bags = run.bags
+        const gratuity = run.gratuity
+        const waiting = run.waiting
+        const date = run.date
+        const resId = run.resId
+        const nameInput = document.getElementById('nameInput').value
+        const enterBtn = document.getElementById('enterBtn')
+    
+        if (nameInput == emp_name){
+        let runPay  = (baseFare * 0.25) + surcharge + (bags * 0.25) + gratuity + (waiting * 0.25)
+        let mainBox = document.getElementById('mainBox')
+        let pastWeek = document.createElement('div')
+        pastWeek.id = ('pastWeek')
+        mainBox.appendChild(pastWeek)
+        let payByRun = document.createElement('div')
+        payByRun.id = 'payByRun'
+        pastWeek.appendChild(payByRun)
+        payByRun.innerHTML = `
+        <p id="block"><br id="value">Driver:</br>${run.emp_name}</p>
+        <p id="block"><br id="value">Run date:</br>${run.date}</p>
+        <p id="block"> <br id="value">Was paid:</br>$${runPay}</p>
+        <p id="block"> <br id="value"> On date:</br>${payPeriod.end}</p>`
+        // let payByWeek = document.createElement('div')
+        // payByWeek.id = 'payByWeek'
+        // pastWeek.appendChild(payByWeek)
+        // payByWeek.innerHTML = `
+        // <p id="block"><br id="value">Driver:</br>${run.emp_name}</p>
+        // <p id="block"> <br id="value">Was paid:</br>$${weekPay}</p>
+        // <p id="block"> <br id="value"> On date:</br>${payPeriod.end}</p>`
+
+      } else {
+        console.log('This is not a driver')
+      }
+    })
+     
+      // console.log(`run on date: ${run.date}, was paid on date ${payPeriod.end}`)
+      assignedToPayPeriod = true
+      break
+    }
+  }
+
+  if (!assignedToPayPeriod) {
+    console.log(`Run on ${run.date} is not within any pay period.`)
+  }
 })
 
 
-//filter by driver
+console.log('Runs by Pay Period:', runsByPayPeriod)
 
 
 
-
-
-
-
-
-
-//CREATE TOTAL AMOUNTS FOR PAYMENTS FROM RUNS DATA
-// for(i=0; i < allRuns.length; i++){
-// if (allRuns.date = pastDates || nextDates){
-//   console.log('subtract 7 days and add totals')
-// } else {
-//   console.log('there is nobody to pay this week')
-// }
-// }
 
 }
 yesRunsPayments()
-
-
-
-
-
-
-
-  //   //GET PAYMENTS
-  // async function paymentsDisplayed{
-  //     const localHost = 'http://localhost:3001/'
-  //     // const displayPayments = async () => {
-  //       await axios.get(`${localHost}payments`, { emp_name: payments.emp_name.value, week_ending: payments.week_ending.value, totalAmount: payments.totalAmount.value, reimbursements: payments.reimbursements.value })
-  //       let pastWeek = document.getElementById('pastWeek')
-  //       let pay = document.createElement('div')
-  //       pay.innerHTML = `
-  //       <p id="block">${sunday}</p>
-  //       <p id="block">Earned: $${monday}</p>
-  //       <p id="block"> On date: ${tuesday}</p>`
-  //       pastWeek.appendChild(pay)
-  // }
-  //   // }
-
-  
-  
-  // getPayments()
